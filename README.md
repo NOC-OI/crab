d5fff33dc898306352361e61082c43a932f5a896
Alex Baldwin <alewin@noc.ac.uk>
2024-12-04 16:17:26 +0000

    Updating readme and launch script

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # C.R.A.B
 A FOSS stack for ML dataset management and annotation
 
+## Prerequisites
+
+> [!NOTE]
+> This repository contains submodules. Make sure to clone with `--recurse-submodules`!
+
+You will need to pre-install Docker Compose and BIIGLE, should you wish to use BIIGLE integrations. Work is ongoing to integrate BIIGLE into this deployment, for now install [BIIGLE](https://biigle-admin-documentation.readthedocs.io/installation/) seperately.
+
 ## Installation
 
 For a local install, run `flask/build.sh` first.
 Then run `generate-env.sh` to auto-generate a `.env` file.
 If you're using externally managed S3 compatible storage, edit the `.env` file now and skip running `init-db.sh`. You'll need to configure databases manually.
 Run `init-db.sh` to prepare your databases automatically.
-Finally, run `docker compose up -d` to start your instance.
+Finally, run `launch.sh` to start your instance.
 CRAB should now be avaliable on `http://localhost:8080`
 
-### Prerequisites
+## Configuration
 
-You will need to pre-install Docker Compose and BIIGLE. Work is ongoing to integrate BIIGLE into this deployment, for now install [BIIGLE](https://biigle-admin-documentation.readthedocs.io/installation/) seperately.
+### HTTPS
+
+While normally, a seperate reverse proxy on the host machine would be used, one can enable SSL on the included nginx container. Edit the config template in `templates/nginx.conf` to enable SSL before launching.
---
+++ launch.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+source .env
+docker compose down
+docker compose up -d minio
+docker compose up -d couchdb
+# In this order since CouchDB usually takes longer than MinIO to start
+echo "Waiting for MinIO and CouchDB to start..."
+pingcount=0
+until docker exec -it crab-minio curl -s -f -o /dev/null "http://localhost:9000/minio/health/live"
+do
+        symbol="|"
+        case $(($pingcount % 4)) in
+        0)
+                symbol="/"
+                ;;
+        1)
+                symbol="-"
+                ;;
+        2)
+                symbol="\\"
+                ;;
+        3)
+                symbol="|"
+                ;;
+        esac
+        secs=$(($pingcount/10))
+        echo -en "\rStill waiting for MinIO to start $symbol [$secs s]"
+        pingcount=$(($pingcount+1))
+        sleep 0.1
+done
+echo ""
+echo "MinIO started!"
+pingcount=0
+until docker exec -it crab-couchdb curl -s -f -o /dev/null "http://$COUCHDB_ROOT_USER:$COUCHDB_ROOT_PASSWORD@localhost:5984/_dbs_info"
+do
+	symbol="|"
+	case $(($pingcount % 4)) in
+	0)
+        	symbol="/"
+        	;;
+	1)
+        	symbol="-"
+        	;;
+	2)
+        	symbol="\\"
+        	;;
+	3)
+        	symbol="|"
+        	;;
+	esac
+	secs=$(($pingcount/10))
+	echo -en "\rStill waiting for CouchDB to start $symbol [$secs s]"
+	pingcount=$(($pingcount+1))
+	sleep 0.1
+done
+echo ""
+echo "CouchDB up!"
+echo "Launching remaining containers..."
+docker compose up -d
+echo "Done!"
