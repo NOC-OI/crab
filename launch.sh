c4de6ad291138b2ff163eafcbc652836eeffd125
Alex Baldwin <alewin@noc.ac.uk>
2024-12-12 10:10:18 +0000

    Fixing deps for docker image

--- launch.sh
+++ launch.sh
@@ -3,6 +3,7 @@ source .env
 docker compose down
 docker compose up -d minio
 docker compose up -d couchdb
+docker compose up -d keycloak
 # In this order since CouchDB usually takes longer than MinIO to start
 echo "Waiting for MinIO and CouchDB to start..."
 pingcount=0
@@ -55,6 +56,33 @@ do
 done
 echo ""
 echo "CouchDB up!"
+echo "Waiting for Keycloak to start..."
+pingcount=0
+until curl -s -f -o /dev/null http://localhost:7080/realms/master/.well-known/openid-configuration
+do
+        symbol="|"
+        case $(($pingcount % 4)) in
+        0)
+                symbol="/"
+                ;;
+        1)
+                symbol="-"
+                ;;
+        2)
+                symbol="\\"
+                ;;
+        3)
+                symbol="|"
+                ;;
+        esac
+        secs=$(($pingcount/10))
+        echo -en "\rStill waiting for Keycloak to start $symbol [$secs s]"
+        pingcount=$(($pingcount+1))
+        sleep 0.1
+done
+echo ""
+echo "Keycloak up!"
+
 echo "Launching remaining containers..."
 docker compose up -d
 echo "Done!"
