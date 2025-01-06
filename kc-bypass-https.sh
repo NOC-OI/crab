ef4cb39bf5ad9041d41bac5032c56452ea8aee56
Alex Baldwin <alewin@noc.ac.uk>
2025-01-06 15:29:24 +0000

    Updating launch scripts

--- kc-bypass-https.sh
+++ kc-bypass-https.sh
@@ -26,8 +26,13 @@ do
 done
 echo ""
 echo "Keycloak up!"
-cd /opt/keycloak/bin/
-./kcadm.sh config credentials --server http://localhost:7080 --realm master --user $KC_BOOTSTRAP_ADMIN_USERNAME --password $KC_BOOTSTRAP_ADMIN_PASSWORD
-./kcadm.sh update realms/master -s sslRequired=NONE
-./kcadm.sh config credentials --server http://localhost:7080 --realm master --user $KC_BOOTSTRAP_ADMIN_USERNAME --password $KC_BOOTSTRAP_ADMIN_PASSWORD
-./kcadm.sh update realms/crab -s sslRequired=NONE
+docker exec -it crab-keycloak /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:7080 --realm master --user $KC_BOOTSTRAP_ADMIN_USERNAME --password $KC_BOOTSTRAP_ADMIN_PASSWORD
+docker exec -it crab-keycloak /opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE
+
+docker exec -it crab-keycloak /opt/keycloak/bin/kcadm.sh create realms -s realm=crab -s enabled=true -o
+docker exec -it crab-keycloak /opt/keycloak/bin/kcadm.sh update realms/crab -s sslRequired=NONE
+docker exec -it crab-keycloak bash -c "/opt/keycloak/bin/kcadm.sh create clients -r crab -s clientId=crab -s 'redirectUris=[\"http://$CRAB_EXTERNAL_HOST:$CRAB_EXTERNAL_PORT/*\", \"http://$CRAB_EXTERNAL_HOST/*\", \"https://$CRAB_EXTERNAL_HOST:$CRAB_EXTERNAL_PORT/*\", \"https://$CRAB_EXTERNAL_HOST/*\"]' -i > /opt/keycloak/crab-kc-cid.txt"
+
+docker exec -it crab-keycloak /opt/keycloak/bin/kcadm.sh create users -r crab -s username=root -s enabled=true
+#docker exec -it crab-keycloak bash -c "/opt/keycloak/bin/kcadm.sh get clients/\$(cat /opt/keycloak/crab-kc-cid.txt | tr -d '\\n')/installation/providers/keycloak-oidc-keycloak-json"
+docker exec -it crab-keycloak /opt/keycloak/bin/kcadm.sh set-password -r crab --username root --password $KC_BOOTSTRAP_ADMIN_PASSWORD --temporary
